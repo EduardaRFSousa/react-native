adfc4484838ad67517b0cb3669cefa5a8da85cc6
Eduarda Rocha <eduardarocha.dev@gmail.com>
2025-09-05 11:44:58 -0300

    commit testando componentes

--- myapp/App.tsx
+++ myapp/App.tsx
@@ -1,11 +1,21 @@
+// Componentes utilizados para construir a página, importados do React Native
+import { StyleSheet, Text, View, TextInput, Image } from 'react-native';
+// Componente StatusBar serve para configurar a barrinha superior da tela (Experimente trocar o style 'dark' por 'light')
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
+// ATENÇÃO: Todas as tags de componentes começam em maiúsculo!
+/* Sobre o RETURN da function App (função de chamada da aplicação):
+O return só pode retornar um único componente (<View>), que é 
+uma div gigante que funcionacomo o elemento <html> */
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* Comentários dentro do componente view principal são
+       tratados como objeto, ou seja, é obrigatório as ASPAS! */}
+      <TextInput>Escreva aqui!</TextInput>
+      <Text>Hello, World!</Text>
+      <Image source={require('./assets/favicon.png')}></Image>
+      <StatusBar style="dark" />
     </View>
   );
 }
