7405020b7aa6b86cb45341b02a2508b47a4cdf78
Eduarda Rocha <eduardarocha.dev@gmail.com>
2025-09-08 14:17:36 -0300

    commit inicial com comentários

--- myapp/App.tsx
+++ myapp/App.tsx
@@ -1,3 +1,4 @@
+// Hook utilizado para realizar a troca de estado de um componente
 import React, {useState} from 'react';
 // Componentes utilizados para construir a página, importados do React Native
 import { StyleSheet, Text, View, TextInput, Image, TouchableOpacity } from 'react-native';
@@ -7,18 +8,25 @@ import { StatusBar } from 'expo-status-bar';
 // ATENÇÃO: Todas as tags de componentes começam em maiúsculo!
 /* Sobre o RETURN da function App (função de chamada da aplicação):
 O return só pode retornar um único componente (<View>), que é 
-uma div gigante que funcionacomo o elemento <html> */
+uma div gigante que funciona como o elemento <html> */
 export default function App() {
   /* NOTA IMPORTANTE: A alteração de estilo de componentes de forma DINÂMICA 
   em React Native é feita através da mudança de ESTADO ao invés de variáveis comuns, 
   nesse caso é utilizado o useState para criar o estado isActive (padrão false) 
   e a função setIsActive para alterá-lo. */
+
+  /* PADRÃO DE ESCRITA DO USESTATE:
+  - isActive: variável utilizada;
+  - setIsActive: função a ser chamada; 
+  - useState: declaração do hook;
+  - false: valor padrão da variável;*/
   const [isActive, setIsActive] = useState(false);
+  
+  // callback: função anônima dentro de setIsActive() dentro de handlePress()
   function handlePress() {
-    setIsActive((oldValue:boolean) =>{
-      return !oldValue;
+    setIsActive((oldValue:boolean) => { 
+      return !oldValue; 
     });
-    console.log(isActive);
   }
 
   return (
@@ -28,7 +36,7 @@ export default function App() {
       <Text style={isActive? {color: 'white'} : {color: 'black'}}>Troca de estado com React Native</Text>
       <Text style={isActive? {color: 'white'} : {color: 'black'}}>Dica: Clique no coração!</Text>
 
-      {/* Recomendado utilizar requier() ao invés de importar o arquivo*/}
+      {/* Recomendado utilizar require() ao invés de importar o arquivo*/}
       <TouchableOpacity onPress={handlePress}>
         <Image source={isActive ? require('./assets/coracaovermelho.png') : require('./assets/coracaovazio.png')}></Image>
       </TouchableOpacity>
